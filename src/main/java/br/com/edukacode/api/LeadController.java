9bc2d4f2c979fdb26330dc6162120f8d48050bc2
gbL25 <gabriel.alves250597@gmail.com>
2025-09-03 09:05:39 -0300

    Get do Lead

--- src/main/java/br/com/edukacode/api/LeadController.java
+++ src/main/java/br/com/edukacode/api/LeadController.java
@@ -1,5 +1,7 @@
 package br.com.edukacode.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +27,8 @@ public class LeadController {
     }
 
     @GetMapping
-    public void listarLeads() {
-
+    public List<DadosListagemLead> listarLeads() {
+        return repository.findAll().stream().map(DadosListagemLead::new).toList();
     }
 
     @PutMapping
